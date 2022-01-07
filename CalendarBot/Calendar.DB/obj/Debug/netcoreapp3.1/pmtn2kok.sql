76292d86a37bf446046ddb47254ef4f35a3b1c25
cbweiland <cweiland@cord.edu>
2022-01-07 08:41:25 -0600

    Removed Unnecessary files

--- CalendarBot/Calendar.DB/obj/Debug/netcoreapp3.1/pmtn2kok.sql
+++
@@ -1,9 +0,0 @@
-﻿START TRANSACTION;
-
-ALTER TABLE `ChannelConfigs` ADD `GuildId` text NULL;
-
-INSERT INTO `__EFMigrationsHistory` (`MigrationId`, `ProductVersion`)
-VALUES ('20210904162156_GuildID', '5.0.8');
-
-COMMIT;
-
